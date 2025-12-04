bb549445e485e6bc348c5342a835fb2b386bf977
Rifki Afriadi <166176893+rifkiiiafdd@users.noreply.github.com>
2025-12-04 11:28:35 +0700

    fix alu

--- supported/alu_adder_rv32i.v
+++ supported/alu_adder_rv32i.v
@@ -1,17 +1,19 @@
 module alu_adder_rv32i (
-    input  wire [31:0] in1,      // First operand
-    input  wire [31:0] in2,      // Second operand
-    input wire type,
-    output wire [31:0] out     // Sum output
+    input  wire [31:0] in1,
+    input  wire [31:0] in2,
+    input  wire type,      // 0: Add, 1: Sub
+    output wire [31:0] out
 );
-    // Combinational logic for 32-bit adder
-    wire not_in2;
-    assign not_in2 = ~in2 + 1;
+    // Perbaikan: Tambahkan [31:0] agar menjadi 32-bit
+    wire [31:0] not_in2; 
+    
+    // Two's complement: Flip bits + 1
+    assign not_in2 = ~in2 + 1'b1;
 
     wire [31:0] sum;
     wire [31:0] diff;
 
-    assign sum = in1 + in2;
+    assign sum  = in1 + in2;
     assign diff = in1 + not_in2;
 
     assign out = (type == 1'b0) ? sum : diff;
